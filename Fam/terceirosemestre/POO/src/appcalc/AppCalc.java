e0d8c487ffbb6fc28af1bf42cde4a2dce564929b
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-02-14 23:26:02 -0300

    update

--- Fam/terceirosemestre/POO/src/appcalc/AppCalc.java
+++ Fam/terceirosemestre/POO/src/appcalc/AppCalc.java
@@ -4,8 +4,8 @@ public class AppCalc {
 
 	public static void main(String[] args) {
 		System.out.println("******************MENU******************");
-		System.out.println(  "*           Opção 1 - SOMAR            *");
-		System.out.println(  "*           Opção 2 - SUBTRAIR         *");
+		System.out.println("*           Opção 1 - SOMAR            *");
+		System.out.println("*           Opção 2 - SUBTRAIR         *");
 		System.out.println("****************************************");
 	}
 
