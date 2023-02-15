1df63061cfacdfb633f60beef7abd0e08e8310b4
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-02-14 21:47:25 -0300

    Op1 Op2

--- Fam/terceirosemestre/POO/src/appcalc/AppCalc.java
+++ Fam/terceirosemestre/POO/src/appcalc/AppCalc.java
@@ -3,7 +3,10 @@ package appcalc;
 public class AppCalc {
 
 	public static void main(String[] args) {
-		System.out.println("Meu Primeiro Projeto em OO");
+		System.out.println("******************MENU******************");
+		System.out.println(  "*           Opção 1 - SOMAR            *");
+		System.out.println(  "*           Opção 2 - SUBTRAIR         *");
+		System.out.println("****************************************");
 	}
 
 }
