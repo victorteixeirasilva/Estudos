79309523409b4754499b9ef8d6bd3e5dcf79cd45
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-02-20 14:12:44 -0300

    Finalizado

--- Fam/terceirosemestre/POO/AgenciaBancaria/src/Programa/Conta.java
+++ Fam/terceirosemestre/POO/AgenciaBancaria/src/Programa/Conta.java
@@ -10,7 +10,7 @@ public class Conta {
 	private Pessoa pessoa;
 	private Double saldo = 0.0;
 	
-	public Conta(int numeroConta, Pessoa pessoa, Double saldo) {
+	public Conta(Pessoa pessoa) {
 		this.numeroConta = contadorDeContas;
 		this.pessoa = pessoa;
 		contadorDeContas += 1;
