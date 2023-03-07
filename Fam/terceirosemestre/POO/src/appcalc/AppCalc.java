7722b6b5415bcb9929bb0d7663cd64f283d07834
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-03-07 20:26:07 -0300

    ex4

--- Fam/terceirosemestre/POO/src/appcalc/AppCalc.java
+++ Fam/terceirosemestre/POO/src/appcalc/AppCalc.java
@@ -4,7 +4,6 @@
 package appcalc;
 
 import java.util.Scanner;
-
 import operacoes.Operacoes;
 import relatorio.Relatorio;
 //Classe é nomeado como projeto seguindo as boas praticas de programação
@@ -13,56 +12,59 @@ public class AppCalc {
 	public static void main(String[] args) {
 		// A grande maioria dos comandos segue a grafia de primeira letra maiuscula e o
 		// resto do comando em minusculo.
-		System.out.println("BOA NOITE!");
-		System.out.println("DICIPINA POO!");
+		System.out.println("\n=================\n");
+		System.out.println("    BOA NOITE!");
+		System.out.println("   DICIPINA POO!");
+		System.out.println("\n=================\n");
+		menu();
+		System.exit(0);
+
+	}
+	public static void ex4() {
+		//Exercício 4 - Aprovado e Reprovado.
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("\n=================================");
+		System.out.print("Digite a primeira nota: ");
+		float nota1 = entrada.nextFloat();
+		System.out.print("\nDigite a primeira nota: ");
+		float nota2 = entrada.nextFloat();
+		float media = (nota1+nota2)/2;
+		if(media>=6) {
+			System.out.println("\nAluno Aprovado com a média "+media);
+		}
+		System.out.println("=================================\n");
+		entrada.close();
+	}
+	public static void ex2() {
+		Relatorio relatorios = new Relatorio();
+		relatorios.relatorio();
+	}
+	public static void ex3() {
+		//Exercício 3
+		int base, altura, area;
 		
 		Scanner entrada = new Scanner(System.in);
 		
-		System.out.println("******************MENU******************");
-		System.out.println("*           Opção 1 - EXERCÍCIO 1      *");
-		System.out.println("*                  (APPCALC)           *");
-		System.out.println("*           Opção 2 - EXERCÍCIO 2      *");
-		System.out.println("*                  (COMISSÃO)          *");
-		System.out.println("****************************************");
-		int exerc = entrada.nextInt();
-		switch(exerc) {
-		case 1:
-			menu();
-			break;
-		case 2:
-			Relatorio relatorios = new Relatorio();
-			relatorios.relatorio();
-			break;
-		default:
-			System.out.println("Opção Invalida!");
+		System.out.print("Digite a base: ");
+		altura = entrada.nextInt();
+		
+		System.out.print("\nDigite a base: ");
+		base = entrada.nextInt();
+		area = base*altura;
+		if(base==altura) {
+			System.out.println("============================================");
+			System.out.println("É um QUADRADO...");
+			System.out.println("A area do quadrilatero é (base X altura = área) = "+base+" X "+altura+" = "+area);
+			System.out.println("============================================");				
+		} else {
+			System.out.println("============================================");
+			System.out.println("É um RETANGULO...");
+			System.out.println("A area do quadrilatero é (base X altura = área) = "+base+" X "+altura+" = "+area);
+			System.out.println("============================================");
 		}
-		
-		
-		
-		
-		/*
-		System.out.println("******************MENU******************");
-		System.out.println("*           Opção 1 - SOMAR            *");
-		System.out.println("*           Opção 2 - SUBTRAIR         *");
-		System.out.println("****************************************");
-		
-		soma();
-		subtrair();
-		multiplicar();
-		dividir();
-		*/
-		
-		System.exit(0);
-
-		/*
-		 * Anotações
-		 * 
-		 * Codigo com qualidade: -> O ideal para um código com uma melhor performance, é
-		 * com o menor número de variaveis. Pois cada uma delas representa um espaço em
-		 * memoria.
-		 */
+		entrada.close();
 	}
-	public static void menu() {
+	public static void ex1() {
 		Operacoes operacoes = new Operacoes();
 		Scanner entrada = new Scanner(System.in);
 		
@@ -92,53 +94,48 @@ public class AppCalc {
 			break;
 		}
 		System.out.println("");
-		menu();
+		ex1();
+		entrada.close();
+	}	
+	public static void menu() {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("******************MENU******************");
+		System.out.println("*           Opção 1 - EXERCÍCIO 1      *");
+		System.out.println("*           (APPCALC)           *");
+		System.out.println("*           Opção 2 - EXERCÍCIO 2      *");
+		System.out.println("*           (COMISSÃO)          *");
+		System.out.println("*           Opção 3 - EXERCÍCIO 3      *");
+		System.out.println("*           (QUADRADO||RETANGULO)      *");
+		System.out.println("*           Opção 4 - EXERCÍCIO 4      *");
+		System.out.println("*           (APROVADO||REPROVADO)      *");
+		System.out.println("****************************************");
+		int exerc = entrada.nextInt();
+		switch(exerc) {
+		case 1:
+			ex1();
+			break;
+		case 2:
+			ex2();
+			break;
+		case 3:
+			ex3();
+			break;
+		case 4:
+			ex4();
+			break;
+		default:
+			System.out.println("Opção Invalida!");
+			break;
+		}
+		entrada.close();
 	}
 
 	/*
-	public static void soma() {
-		int n1, n2;
-		Scanner entrada = new Scanner(System.in);
-		System.out.print("Digite o valor do Primeiro Número: ");
-		n1 = entrada.nextInt();
-		System.out.print("\nDigite o valor do Segundo Número: ");
-		n2 = entrada.nextInt();
-		System.out.println("\nSOMANDO...");
-		System.out.println("N1:" + n1 + " + N2:" + n2 + " = " + (n1 + n2));
-	}
-
-	public static void subtrair() {
-		int n1, n2;
-		Scanner entrada = new Scanner(System.in);
-		System.out.print("Digite o valor do Primeiro Número: ");
-		n1 = entrada.nextInt();
-		System.out.print("\nDigite o valor do Segundo Número: ");
-		n2 = entrada.nextInt();
-		System.out.println("\nSUBTRAINDO...");
-		System.out.println("N1:" + n1 + " - N2:" + n2 + " = " + (n1 - n2));
-	}
-
-	public static void multiplicar() {
-		int n1, n2;
-		Scanner entrada = new Scanner(System.in);
-		System.out.print("Digite o valor do Primeiro Número: ");
-		n1 = entrada.nextInt();
-		System.out.print("\nDigite o valor do Segundo Número: ");
-		n2 = entrada.nextInt();
-		System.out.println("\nMULTIPLICANDO...");
-		System.out.println("N1:" + n1 + " x N2:" + n2 + " = " + (n1 * n2));
-	}
-
-	public static void dividir() {
-		int n1, n2;
-		Scanner entrada = new Scanner(System.in);
-		System.out.print("Digite o valor do Primeiro Número: ");
-		n1 = entrada.nextInt();
-		System.out.print("\nDigite o valor do Segundo Número: ");
-		n2 = entrada.nextInt();
-		System.out.println("\nDIVIDINDO...");
-		System.out.println("N1:" + n1 + " / N2:" + n2 + " = " + (n1 / n2));
-	}
-	*/
-
+	 * Anotações
+	 * 
+	 * Codigo com qualidade: -> O ideal para um código com uma melhor performance, é
+	 * com o menor número de variaveis. Pois cada uma delas representa um espaço em
+	 * memoria.
+	 */
 }
