703ceeeb377b80dda9efe1bd2de5f25774bb32cd
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-03-21 20:32:29 -0300

    aula poo

--- Fam/terceirosemestre/POO/Principal/src/principal/Operacoes.java
+++ Fam/terceirosemestre/POO/Principal/src/principal/Operacoes.java
@@ -1,5 +1,41 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Operacoes {
+	int n1, n2;
+	Scanner entrada = new Scanner(System.in);
+	
+	public void Menu() {		
+		System.out.println("******************MENU******************");
+		System.out.println("*           Opção 1 - SOMAR            *");
+		System.out.println("*           Opção 2 - SUBTRAIR         *");
+		System.out.println("*           Opção 3 - MULTIPLICAR      *");
+		System.out.println("*           Opção 4 - DIVIDIR          *");
+		System.out.println("****************************************");
+		System.out.print("Opção: ");
+		int escolha = entrada.nextInt();
+		switch(escolha) {
+		case 1:
+			
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		default:
+			System.out.println("Opção invalida!");
+			break;
+		}
+	}
+	
+	public void entrarNumeros() {
+		
+	}
 
 }
---
+++ Fam/terceirosemestre/POO/Principal/src/principal/Principal.java
@@ -0,0 +1,14 @@
+package principal;	
+
+public class Principal {
+
+	
+	public static void main(String[] args) {
+		
+		Operacoes op = new Operacoes();
+		
+		op.Menu();
+		
+	}
+
+}
