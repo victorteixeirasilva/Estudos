1317685564c8c67add806bad728b119ca2ba0fcd
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-03-29 15:20:40 -0300

    holerite

--- Fam/terceirosemestre/POO/ProjetoHolerite/src/br/fam/poo/Main.java
+++ Fam/terceirosemestre/POO/ProjetoHolerite/src/br/fam/poo/Main.java
@@ -1,11 +1,11 @@
 package br.fam.poo;
 
-import br.fam.poo.Calculo;
-
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Calculo calculo = new Calculo();
+		
+		calculo.entrarNome();
 
 	}
 
