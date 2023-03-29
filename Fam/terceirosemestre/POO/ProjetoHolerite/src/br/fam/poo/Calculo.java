343e29c594d83b749638bdc168edccf354d0b020
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-03-28 21:41:43 -0300

    holerite

---
+++ Fam/terceirosemestre/POO/ProjetoHolerite/src/br/fam/poo/Calculo.java
@@ -0,0 +1,21 @@
+package br.fam.poo;
+
+import java.util.Scanner;
+
+public class Calculo {
+	
+	private double sal_atual, tx_aumento, sal_novo, tx_inss, vlr_ir, sal_final, vlr_aumento;
+	private String nome;
+	private Scanner entrada = new Scanner(System.in);
+	
+	public double CalcAumentoSalarial(double tx_aumento, double sal_atual) {
+		
+		return 0;
+	}
+	
+	public void exibirHolerite(double sal_novo, double vlr_inss, double vlr_ir, double sal_final, double salario_liq, String nome) {
+		
+	}
+	
+	
+}
---
+++ Fam/terceirosemestre/POO/ProjetoHolerite/src/br/fam/poo/Main.java
@@ -0,0 +1,12 @@
+package br.fam.poo;
+
+import br.fam.poo.Calculo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
