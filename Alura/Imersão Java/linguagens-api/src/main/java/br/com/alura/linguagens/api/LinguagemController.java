1f3f3301867b1f2021d267c3b63c90d84b73e853
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-04-07 16:45:42 -0300

    bb

--- Alura/Imersão Java/linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ Alura/Imersão Java/linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -3,7 +3,9 @@ package br.com.alura.linguagens.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,15 @@ public class LinguagemController {
 		return linguagemSalva;
 	}
 	
+	//@DeleteMapping("/linguagens")
+	//public Linguagem deletarLinguagem(@RequestBody Linguagem linguagem) {
+		//Linguagem linguagemDeletada = repositorio.delete(linguagem);
+		//return linguagemDeletada;
+	//}
+	
+	@DeleteMapping("/linguagens")
+    public void deletarLinguagem(@PathVariable("id") Linguagem id) {
+        this.repositorio.delete(id);
+    }
 	
 }
