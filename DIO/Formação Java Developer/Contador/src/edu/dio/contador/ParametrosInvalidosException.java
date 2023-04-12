29ea5bc38a41c6e3441bab05a1eb9350202b53cd
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-04-12 15:39:02 -0300

    contador

---
+++ DIO/Formação Java Developer/Contador/src/edu/dio/contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package edu.dio.contador;
+
+public class ParametrosInvalidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ DIO/Formação Java Developer/src/edu/dio/Contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package edu.dio.Contador;
+
+public class ParametrosInvalidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
