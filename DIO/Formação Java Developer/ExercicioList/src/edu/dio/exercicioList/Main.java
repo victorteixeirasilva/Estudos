c44767356ff3ae8f5e5de0d2fa0949b2947c2298
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-05-19 22:32:51 -0300

    Fim

---
+++ DIO/Formação Java Developer/ExercicioList/src/edu/dio/exercicioList/Main.java
@@ -0,0 +1,73 @@
+package edu.dio.exercicioList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {	
+		
+		
+		/*EXERCICIO PROPOSTO 1
+		//Recebe a média dos 6 primeiros meses do ano e armazene-os em uma lista.
+		
+		int mesCount = 1;
+		Scanner entrada = new Scanner(System.in);
+		
+		List<Mes> meses = new ArrayList<>();
+		
+		for(int i = 1; i<=6; i++) {
+			System.out.println("Informe a temperatura média do mês " + mesCount);
+			Double tempMedia = entrada.nextDouble();
+			mesCount++;
+			if (mesCount == 1) {
+				Mes mes = new Mes("JANEIRO", tempMedia);
+				meses.add(mes);
+			} else if (mesCount == 2) {
+				Mes mes = new Mes("FEVEREIRO", tempMedia);
+				meses.add(mes);
+			} else if (mesCount == 3) {
+				Mes mes = new Mes("MARÇO", tempMedia);
+				meses.add(mes);
+			} else if (mesCount == 4) {
+				Mes mes = new Mes("ABRIL", tempMedia);
+				meses.add(mes);
+			} else if (mesCount == 5) {
+				Mes mes = new Mes("MAIO", tempMedia);
+				meses.add(mes);
+			} else {
+				Mes mes = new Mes("JUNHO", tempMedia);
+				meses.add(mes);
+			} 
+		}
+		
+		//Calcula a média semestral das temperaturas
+		Iterator<Mes> iterator = meses.iterator();
+		
+		Double soma = 0.0;
+		Double mediaSemestre = 0.0;
+		
+		while(iterator.hasNext()) {
+			Mes next = iterator.next();
+			soma += next.getTemperaturaMedia();
+		}
+		mediaSemestre = (soma/6);
+		
+		System.out.println("A temperatura média desse semestre foi "+mediaSemestre);
+		
+		
+		//Mostrar todas as temperaturas acima desta média, e em que mês elas ocorreram
+		Iterator<Mes> iterator2 = meses.iterator();
+		
+		while(iterator2.hasNext()) {
+			Mes next = iterator2.next();
+			if(next.getTemperaturaMedia() >= mediaSemestre) {
+				System.out.println("Mês: "+next.getNomeMes()+", Temperatura: "+next.getTemperaturaMedia());
+			}
+		}
+		 */
+	}
+}
+
+
---
+++ DIO/Formação Java Developer/ExercicioList/src/edu/dio/exercicioList/Mes.java
@@ -0,0 +1,21 @@
+package edu.dio.exercicioList;
+
+public class Mes {
+	private String nomeMes;
+	private Double temperaturaMedia;
+	
+	public Mes(String nomeMes, Double temperaturaMedia) {
+		this.nomeMes = nomeMes;
+		this.temperaturaMedia = temperaturaMedia;
+	}
+	
+	public String getNomeMes() {
+		return nomeMes;
+	}
+	public Double getTemperaturaMedia() {
+		return temperaturaMedia;
+	}
+	
+	
+	
+}
