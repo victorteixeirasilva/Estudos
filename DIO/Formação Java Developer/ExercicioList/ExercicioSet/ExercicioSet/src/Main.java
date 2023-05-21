d0c36540338e19a8fc7aaaeb9b9114e4a368dc2d
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-05-21 13:42:12 -0300

    Set Ex 1

---
+++ DIO/Formação Java Developer/ExercicioList/ExercicioSet/ExercicioSet/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> arcoiris = new HashSet<>();
+        arcoiris.add("Vermelho");
+        arcoiris.add("Vermelho");
+    }
+}
