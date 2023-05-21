55c33946b4ddeac8b1fdf0fa3910b6700812f463
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-05-21 13:34:26 -0300

    Fim ex list 2

--- DIO/Formação Java Developer/ExercicioList/src/edu/dio/exercicioList/Main.java
+++ DIO/Formação Java Developer/ExercicioList/src/edu/dio/exercicioList/Main.java
@@ -30,13 +30,47 @@ Se a pessoa responder positivamente a 2 questões ela deve ser classificada como
 		
 		System.out.print("Esteve no local do crime? ");
 		resposta = entrada.next();
-		respostas.add(resposta);
+		respostas.add(resposta.toLowerCase());
 		
 		System.out.print("Mora perto da vítima? ");
 		resposta = entrada.next();
-		respostas.add(resposta);
+		respostas.add(resposta.toLowerCase());
 		
-		System.out.println("");
+		System.out.print("Devia para vítima? ");
+		resposta = entrada.next();
+		respostas.add(resposta.toLowerCase());
+		
+		System.out.print("Já trabalhou com a vítima? ");
+		resposta = entrada.next();
+		respostas.add(resposta.toLowerCase());
+		
+		System.out.println(respostas);
+		
+		int count = 0;
+		Iterator<String> contador = respostas.iterator();
+		
+		while(contador.hasNext()) {
+			String resp = contador.next();
+			if(resp.contains("s")) {
+				count++;
+			}
+		}
+		
+		switch(count) {
+			case 2:
+				System.out.println(">> SUSPEITA <<");
+				break;
+			case 3:
+			case 4:
+				System.out.println(">> CÚMPLICE <<");
+				break;
+			case 5:
+				System.out.println(">> ASSASINO <<");
+				break;
+			default:
+				System.out.println(">> INOCENTE <<");
+				break;
+		}
 		
 		/*EXERCICIO PROPOSTO 1
 		//Recebe a média dos 6 primeiros meses do ano e armazene-os em uma lista.
