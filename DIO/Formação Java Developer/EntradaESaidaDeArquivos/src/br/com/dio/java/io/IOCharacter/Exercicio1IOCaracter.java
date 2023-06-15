419d5a45ff7a3289003b2be2d388e28408ac9a61
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-06-15 19:51:42 -0300

    IO

---
+++ DIO/Formação Java Developer/EntradaESaidaDeArquivos/src/br/com/dio/java/io/IOCharacter/Exercicio1IOCaracter.java
@@ -0,0 +1,11 @@
+package br.com.dio.java.io.IOCharacter;
+
+public class Exercicio1IOCaracter {
+	
+	public static void receberTecladoImprimirConsole() {
+		
+		System.out.println("Recomende 3 Filmes:");
+	}
+	
+	
+}
