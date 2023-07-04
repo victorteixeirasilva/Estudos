66328d139cb5a03e6ff2863f130654b3412e2ab7
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-07-03 22:07:49 -0300

    Spring Primeiros Passos

---
+++ DIO/Formação Java Developer/primeiros-passos/src/main/java/dio/primeirospassos/PrimeirosPassosApplication.java
@@ -0,0 +1,26 @@
+package dio.primeirospassos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassosApplication.class, args);
+
+
+/*
+		Calculadora calculadora = new Calculadora();
+		System.out.println("O resultado é " + calculadora.somar(2,7));
+*/
+
+
+	}
+
+
+
+
+
+
+}
