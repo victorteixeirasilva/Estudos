087b2f34c1f5576211777e339047c45173e88640
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-07-04 09:09:25 -0300

    123

---
+++ DIO/Formação Java Developer/spring-ioc-di-beans-autowired/src/main/java/dio/springiocdibeansautowired/SpringIocDiBeansAutowiredApplication.java
@@ -0,0 +1,13 @@
+package dio.springiocdibeansautowired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocDiBeansAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocDiBeansAutowiredApplication.class, args);
+	}
+
+}
