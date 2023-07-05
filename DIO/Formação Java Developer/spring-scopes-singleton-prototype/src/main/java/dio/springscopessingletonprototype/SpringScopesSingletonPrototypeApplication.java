e2b9bdc382220224599734490732997b295c79e2
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-07-04 21:46:06 -0300

    2

---
+++ DIO/Formação Java Developer/spring-scopes-singleton-prototype/src/main/java/dio/springscopessingletonprototype/SpringScopesSingletonPrototypeApplication.java
@@ -0,0 +1,25 @@
+package dio.springscopessingletonprototype;
+
+import dio.springscopessingletonprototype.app.SistemaMensagem;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringScopesSingletonPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScopesSingletonPrototypeApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(SistemaMensagem sistema) throws Exception{
+		return args -> {
+			sistema.enviarConfirmacaoCadastro();
+			sistema.enviarMensagemBoasVindas();
+			sistema.enviarConfirmacaoCadastro();
+		};
+	}
+
+}
