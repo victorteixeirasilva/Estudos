46108f93b5b25981db808a2bb5540fe136a24952
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-07-06 16:58:08 -0300

    swagger fdp

---
+++ DIO/Formação Java Developer/springboot-web-swagger/src/main/java/dio/springbootwebswagger/SpringbootWebSwaggerApplication.java
@@ -0,0 +1,13 @@
+package dio.springbootwebswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebSwaggerApplication.class, args);
+	}
+
+}
---
+++ DIO/Formação Java Developer/springboot-web-swagger/src/test/java/dio/springbootwebswagger/SpringbootWebSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springbootwebswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
