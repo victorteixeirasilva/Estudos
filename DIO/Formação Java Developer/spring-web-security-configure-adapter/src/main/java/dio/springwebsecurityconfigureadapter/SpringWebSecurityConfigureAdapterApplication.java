0e7ba4dd7816793efa0173f1b1c8cb1c65b18bd5
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-07-06 22:50:23 -0300

    aaaa

---
+++ DIO/Formação Java Developer/spring-web-security-configure-adapter/src/main/java/dio/springwebsecurityconfigureadapter/SpringWebSecurityConfigureAdapterApplication.java
@@ -0,0 +1,13 @@
+package dio.springwebsecurityconfigureadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSecurityConfigureAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSecurityConfigureAdapterApplication.class, args);
+	}
+
+}
---
+++ DIO/Formação Java Developer/spring-web-security-configure-adapter/src/test/java/dio/springwebsecurityconfigureadapter/SpringWebSecurityConfigureAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springwebsecurityconfigureadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSecurityConfigureAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
