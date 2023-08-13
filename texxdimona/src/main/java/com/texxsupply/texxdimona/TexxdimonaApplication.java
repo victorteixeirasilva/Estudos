f23fd3a2937f9457508e5e324c410a18cdaf0365
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-08-13 00:03:37 -0300

    aaaaa

---
+++ texxdimona/src/main/java/com/texxsupply/texxdimona/TexxdimonaApplication.java
@@ -0,0 +1,13 @@
+package com.texxsupply.texxdimona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexxdimonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexxdimonaApplication.class, args);
+	}
+
+}
