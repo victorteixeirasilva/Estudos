97341bc86823a8755c4e8c148bab3cbe4bb5b168
victorteixeirasilva <victorteixeira2308@gmail.com>
2024-05-22 20:21:24 -0300

    att

--- DIO/ProjetosDIO/Desenvolvendo-Microsserviço-em-Java-para-Integração-com-IA-Generativa/avatar/src/main/java/infrastructure/resources/CustomerResource.java
+++ DIO/ProjetosDIO/Desenvolvendo-Microsserviço-em-Java-para-Integração-com-IA-Generativa/avatar/src/main/java/infrastructure/resources/CustomerResource.java
@@ -2,10 +2,16 @@ package infrastructure.resources;
 
 import application.ApplicationService;
 import application.dto.Customer;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import application.dto.ProfilePhoto;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.ResponseStatus;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Path("customers")
 public class CustomerResource {
@@ -18,6 +24,24 @@ public class CustomerResource {
     @GET
     public List<Customer> searchCustomers(){
         return  service.searchCustomers();
+    }
+
+    @Path("/{id}")
+    public Customer getCustomer(@PathParam("id") String id){
+        try {
+            return  service.getCustomer(id);
+        } catch (NoSuchElementException exception) {
+            throw new NotFoundException();
+        }
+    }
+
+    @POST
+    @Path("/{id}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @ResponseStatus(RestResponse.StatusCode.ACCEPTED)
+    public void persistProfilePhoto(@PathParam("id") String id,
+                                    @RestForm("photo")FileUpload fileUpload) {
+        service.persistProfilePhoto(id, ProfilePhoto.create(fileUpload));
     }
 
 }
